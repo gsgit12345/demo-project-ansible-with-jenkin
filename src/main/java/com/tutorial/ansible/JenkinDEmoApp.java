2faba9de62efaa9f791c72e96b979e0cf42f3db5
ghanshyam shukla <gsgmca682@gmail.com>
2024-10-08 08:26:37 +0530

    second committ

---
+++ src/main/java/com/tutorial/ansible/JenkinDEmoApp.java
@@ -0,0 +1,16 @@
+package com.tutorial.ansible;
+
+/**
+ * Hello world!
+ *
+ */
+public class JenkinDEmoApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello hhhh!  hello how  jj" );
+        System.out.println( "Hello hhhh!  hello how  jj" );
+
+    }
+}
+
